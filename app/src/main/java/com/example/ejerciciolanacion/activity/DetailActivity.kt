f5d2c7f1294544feb20f59460de8b64b2ae0c0de
Andrés Torrico <atorrico@ocasadomain.com>
2019-05-23 13:43:15 -0300

    Animaciones

--- app/src/main/java/com/example/ejerciciolanacion/activity/DetailActivity.kt
+++ app/src/main/java/com/example/ejerciciolanacion/activity/DetailActivity.kt
@@ -5,29 +5,34 @@ import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.ejerciciolanacion.R
-import com.example.ejerciciolanacion.adapter.AlbumAdapter
 import com.example.ejerciciolanacion.adapter.PhotoAdapter
-import com.example.ejerciciolanacion.model.Album
 import com.example.ejerciciolanacion.model.Photo
 import com.example.ejerciciolanacion.view_model.AlbumViewModel
 import com.example.ejerciciolanacion.view_model.AlbumViewModelFactory
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_detail.*
+import com.example.ejerciciolanacion.recyclerview.GridRecyclerView
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
+
 
 class DetailActivity : AppCompatActivity() {
     var adapter: PhotoAdapter? = null
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView: GridRecyclerView
+    //private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+
         recyclerView = this.findViewById(R.id.rvPhotos)
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = GridLayoutManager(this,3)
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
+        val resId = R.anim.grid_layout_animation_from_bottom
+        val animation = AnimationUtils.loadLayoutAnimation(this, resId)
+        recyclerView.layoutAnimation = animation
 
         val idAlbum = intent.getStringExtra("idAlbum")
         getViewModel(idAlbum)
